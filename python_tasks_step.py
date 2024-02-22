8111dcea95fba7e35344dc2f95d79ff5ecb92cb7
Ivan-Niki <iFankov@gmail.com>
2024-02-22 23:27:18 +0300

    add middle number task v2

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -666,3 +666,14 @@ elif num2 > num1 and num1 > num3 or num2 < num1 and num1 < num3:
     print(num1)
 elif num2 > num3 and num3 > num1 or num2 < num3 and num3 < num1:
     print(num3)
+
+# 2-й вариант
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+if num1 > num2 > num3 or num1 < num2 < num3:
+    print(num2)
+elif num2 > num1 > num3 or num2 < num1 < num3:
+    print(num1)
+elif num2 > num3 > num1 or num2 < num3 < num1:
+    print(num3)
