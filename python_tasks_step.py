054208b3edc327de54b146ef171da55452ee33d2
Ivan-Niki <iFankov@gmail.com>
2024-03-06 23:17:06 +0300

    add Euclidean distance task code

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -1280,3 +1280,9 @@ else:
 Формат выходных данных:
 Программа должна вывести одно число – евклидово расстояние. """
 
+from math import *
+x1 = float(input())
+y1 = float(input())
+x2 = float(input())
+y2 = float(input())
+print(sqrt((x1 - x2)**2 + (y1 - y2)**2))
