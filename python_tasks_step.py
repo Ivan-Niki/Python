5c2539302cdf4b64f41a38eca288a39966aa06ad
Ivan-Niki <iFankov@gmail.com>
2024-03-16 23:47:14 +0300

    update largest numbers task

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -1668,4 +1668,17 @@ print(total)
 Формат выходных данных:
 Программа должна вывести два наибольших числа, каждое на отдельной строке. """
 
+n = int(input())
+largest1 = 0
+largest2 = 0
+for i in range(n):
+    num = int(input())
+    if num > largest1:
+        largest2 = largest1
+        largest1 = num 
+    elif num > largest2:
+        largest2 = num
+print(largest1)
+print(largest2)
+
 
