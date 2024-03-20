056a5b4bcecf338fe8e4c04f89b1476b8e53b31f
Ivan-Niki <iFankov@gmail.com>
2024-03-20 23:42:38 +0300

    add max-min task code

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -1862,3 +1862,16 @@ while num != 0:
 На вход программе подается одно натуральное число.
 Формат выходных данных:
 Программа должна вывести максимальную и минимальную цифры введенного числа (с поясняющей надписью).
+
+num = int(input())
+min_digit = 9
+max_digit = 0
+while num != 0:
+    last_digit = num % 10
+    if last_digit > max_digit:
+        max_digit = last_digit
+    if last_digit < min_digit:
+        min_digit = last_digit
+    num = num // 10  
+print('Максимальная цифра равна', max_digit) 
+print('Минимальная цифра равна', min_digit)
