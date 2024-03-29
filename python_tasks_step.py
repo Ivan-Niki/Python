9f2622222b351f5a7f336c8b9780df58304b03f8
Ivan-Niki <iFankov@gmail.com>
2024-03-29 23:35:22 +0300

    add 136th task

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -2011,3 +2011,12 @@ print(result)
 
 # Ответ: 25
 
+# 136.  Что покажет приведенный ниже фрагмент кода?
+mult = 1
+for i in range(1, 11):
+   if i % 2 == 0:
+      continue
+   if i % 9 == 0:
+      break
+   mult *= i
+print(mult)
