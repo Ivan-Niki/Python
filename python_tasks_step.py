bd50ae91f15b0e0360173c03244197d511277fe7
Ivan-Niki <iFankov@gmail.com>
2024-04-01 23:56:09 +0300

    add 139-task final code

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -2076,3 +2076,16 @@ if count > 0:
 else:
     print('NO')
 
+# Исправленный вариант кода:
+count = 0
+p = 1
+for i in range(1, 11):
+    x = int(input())
+    if x >= 0:
+        p = p * x
+        count = count + 1
+if count > 0:
+    print(count)
+    print(p)
+else:
+    print('NO')
