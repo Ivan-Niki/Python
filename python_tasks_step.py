7220126560dd303424fdec38ccbf594f1bc19e74
Ivan-Niki <iFankov@gmail.com>
2024-04-04 23:51:47 +0300

    add 142th task initial code

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -2162,3 +2162,16 @@ print(total_sum)
 Примечание 1. Число 0 делится на любое натуральное число.
 Примечание 2. При необходимости вы можете добавить нужные строки кода. """
 
+# Исходный вариант кода:
+n = int(input())
+max_digit = n % 10
+while n > 0:
+    digit = n % 10
+    if digit % 3 == 0:
+        if digit < max_digit:
+            digit = max_digit
+    n = n % 10
+if max_digit == 0:
+    print('NO')
+else:
+    print(max_digit)
