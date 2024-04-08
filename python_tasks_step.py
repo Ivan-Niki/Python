20559757f8629a88ed2e4d1f3abd19d82b66d1e2
Ivan-Niki <iFankov@gmail.com>
2024-04-09 00:02:18 +0300

    add stars-triangle 151th task code

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -2321,3 +2321,14 @@ for i in range(1, n + 1):
 Программа должна вывести треугольник в соответствии с условием.
 Примечание. Используйте вложенный цикл for. """
 
+n = int(input())
+mid = n // 2 + 1
+for i in range(1, mid + 1):
+    for j in range(i):
+        print('*', end='')
+    print()
+for k in range(mid - 1, 0, -1):
+    for m in range(k):
+        print('*', end='')
+    print()
+
