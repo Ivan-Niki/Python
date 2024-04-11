bdbff62961f106be6ec4e1b7f5595563e8f1a1c9
Ivan-Niki <iFankov@gmail.com>
2024-04-11 23:58:05 +0300

    add 157th task

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -2379,3 +2379,7 @@ s = "In 2010, someone paid 10k Bitcoin for two pizzas."
 print(s[])
 
 # Ответ: print(s[7])
+
+157. Дополните приведенный код, используя индексатор, так чтобы он вывел символ w.
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[])
