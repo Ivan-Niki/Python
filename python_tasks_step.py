5f69fbf6be1d7abe22bcd9c3cff2807f885da1b7
Ivan-Niki <iFankov@gmail.com>
2024-04-11 23:57:40 +0300

    add 156th task answer

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -2377,3 +2377,5 @@ for i in range(0, len(s), 5):
 # 156. Дополните приведенный код, используя индексатор, так чтобы он вывел символ запятой.
 s = "In 2010, someone paid 10k Bitcoin for two pizzas."
 print(s[])
+
+# Ответ: print(s[7])
