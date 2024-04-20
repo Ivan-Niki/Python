07766232349b95697a63745fe9de35f83521e991
Ivan-Niki <iFankov@gmail.com>
2024-04-20 23:52:04 +0300

    add 175th task

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -2584,3 +2584,7 @@ s = "In 2010, someone paid 10k Bitcoin for two pizzas."
 print()
 # Ответ: print(s[::7])
 
+# 175. Дополните приведенный код, используя срезы, так чтобы он вывел строку s в обратном порядке.
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print()
+
