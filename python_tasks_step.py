4b04886ee16373f5909e9cfd2f08eb0adc1a3947
Ivan-Niki <iFankov@gmail.com>
2024-05-06 23:32:26 +0300

    add 193th task

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -2769,6 +2769,11 @@ s = 'www.stepik.org'
 print(s.endswith('.ru'))
 # Ответ: False
 
+# 193. Что покажет приведенный ниже фрагмент кода?
+s = 'I learn Python language. Python - awesome!'
+print(s.find('Python'))
+
+
 
 
 
