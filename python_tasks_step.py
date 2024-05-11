6825930c4cabda98cc7e7f1b30f47c0855bbb607
Ivan-Niki <iFankov@gmail.com>
2024-05-11 23:43:10 +0300

    add 205th task answer

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -2929,10 +2929,18 @@ print(s.format(year, cost, currency))
 # 205. Дополните приведенный код, используя форматирование строк с помощью f-строк, так чтобы он вывел текст: 
 «In 2010, someone paid 10K Bitcoin for two pizzas.» (без кавычек).
 
+# Условие:
 year = 2010
 amount = '10K'
 currency = 'Bitcoin'
 
 print('In {}, someone paid {} {} for two pizzas.')
+
+# Решение:
+year = 2010
+amount = '10K'
+currency = 'Bitcoin'
+
+print(f'In {year}, someone paid {amount} {currency} for two pizzas.')
 
 
