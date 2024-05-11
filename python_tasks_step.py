aea86f47452cc64f10662256187c9cb01355f5dc
Ivan-Niki <iFankov@gmail.com>
2024-05-11 23:40:14 +0300

    update 204th task

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -2913,4 +2913,9 @@ print(result_char)
 # 204. Дополните приведенный код, используя форматирование строк с помощью метода format, так чтобы он вывел текст: 
 «In 2010, someone paid 10k Bitcoin for two pizzas.» (без кавычек).
 
+# Условие:
+s = 'In {0}, someone paid {1} {2} for two pizzas.'
+
+print()
+
 
