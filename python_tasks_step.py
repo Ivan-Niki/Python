0786f4b902662f9d3e679f819addd1735a0517cc
Ivan-Niki <iFankov@gmail.com>
2024-05-11 23:41:41 +0300

    add 205th task

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -2926,3 +2926,8 @@ currency = 'Bitcoin'
 print(s.format(year, cost, currency))
 
 
+# 205. Дополните приведенный код, используя форматирование строк с помощью f-строк, так чтобы он вывел текст: 
+«In 2010, someone paid 10K Bitcoin for two pizzas.» (без кавычек).
+
+
+
