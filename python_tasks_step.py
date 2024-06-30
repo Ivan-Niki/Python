faecea7f8c3dc7714adecbc9b35b26cef7f7c453
Ivan-Niki <iFankov@gmail.com>
2024-06-30 23:16:44 +0300

    add 271th task rstrip method answer

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -3794,3 +3794,6 @@ print(s.lstrip())
 # 271. Что будет результатом выполнения следующего кода:
 s = '      foo bar foo baz foo qux      '
 print(s.rstrip())
+
+# Ответ:
+'      foo bar foo baz foo qux'
