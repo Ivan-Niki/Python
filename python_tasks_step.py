db126689aff807b82de923b7509314f601c31054
Ivan-Niki <iFankov@gmail.com>
2024-07-13 23:57:29 +0300

    add format-method exmple

--- python_tasks_step.py
+++ python_tasks_step.py
@@ -3949,9 +3949,18 @@ print(txt)
 # Результатом выполнения такого кода будет:
 My name is Timur-Timur-Timur
 
+
 #   ========== f-строки ==========
 """ Метод format хорошо справляется с задачей форматирования строк, однако если параметров много, то код 
-может показаться немного избыточным:
+может показаться немного избыточным: """
+first_name = 'Timur'
+last_name = 'Guev'
+age = 27
+profession = 'math teacher'
+affiliation = 'BeeGeek'
+print('Hello, {0} {1}. You are {2}. You are a {3}. You were a member of {4}'
+               .format(first_name, last_name, age, profession, affiliation))
+
 
 
 
